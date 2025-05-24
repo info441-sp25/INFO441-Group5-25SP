13efe0b924381e088b8eceb4569688314f466315
Joseph Esteban <jesteban@uw.edu>
2025-05-23 23:31:51 -0700

    adjusted port constant to always be 3000, not enviromment - je

--- backend/app.js
+++ backend/app.js
@@ -100,7 +100,7 @@ app.use(authProvider.interactionErrorHandler());
 
 // backend connection to do
 
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 export default app;
