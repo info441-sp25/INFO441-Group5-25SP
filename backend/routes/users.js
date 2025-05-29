1c435b21e72cec2fb7db005f4988d1b2cf975928
Amelia <amelialx99@gmail.com>
2025-05-29 01:28:16 -0700

    feat: get endpoint for user, create and save user to mongodb

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,4 +1,6 @@
 import express from "express";
+import models from '../models.js';
+
 var router = express.Router();
 
 /* GET users listing. */
@@ -18,6 +20,28 @@ router.get("/myIdentity", function (req, res, next) {
       res.json({ status: "loggedout" })
   }
 });
+
+router.get('/:username', async (req, res) => {
+  try {
+    console.log('Getting user info for ' + req.params.username);
+    const user = await models.User.findOne({ username: req.params.username });
+
+    if (!user) {
+      const newUserInfo = new models.User({
+        username: req.params.username,
+        createdCrosswords: [],
+        savedCrosswords: []
+      });
+      await newUserInfo.save();
+      console.log('New user info saved');
+      return res.json(newUserInfo)
+    }
+    res.json({user})
+  } catch (error) {
+    console.error('Error getting user info:', error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+})
 
 // Test route, simulate authentication
 router.post('/test-auth', function(req, res) {
